9290db26dfbd54dcd64c3a0cdd65f4a85fccffea
Cristian Alegre Bustos <cnalegrebustos@teco.com.ar>
2023-04-24 17:01:02 -0300

    feat: agregar style de forma global exitosamente

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,1 +1,18 @@
-import "src/style.scss"
+import "src/styles/index.scss"
+import "src/styles/indexTailwind.css"
+import { useRouter } from 'next/router'
+import { AppProps } from "next/app";
+
+const App = ({ Component, pageProps }: AppProps) => {
+    const router = useRouter()
+
+  
+    return (
+      <>
+        <Component {...pageProps} />
+      </>
+    )
+  }
+  
+  export default App
+  
