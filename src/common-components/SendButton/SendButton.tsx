81d6cc52910881d555f7e0f5f9b6a4e0e03f3045
Cristian Alegre Bustos <cnalegrebustos@teco.com.ar>
2023-04-28 13:53:44 -0300

    feat: color de boton enviar

--- src/common-components/SendButton/SendButton.tsx
+++ src/common-components/SendButton/SendButton.tsx
@@ -1,7 +1,7 @@
 const SendButton =()=>{
     return(
         <button
-        className="ml-4 bg-red-metallic rounded py-2 px-4 "
+        className="ml-4 bg-amber-500 rounded py-2 px-4 "
         type="submit"
       >
         Enviar
