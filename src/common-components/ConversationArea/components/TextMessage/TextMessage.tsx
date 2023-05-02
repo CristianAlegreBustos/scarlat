647bed3bb07cd2af7614dfeceaf3301a73af98c0
Cristian Alegre Bustos <cnalegrebustos@teco.com.ar>
2023-05-02 16:04:31 -0300

    feat: optimizaction TextMessage

--- src/common-components/ConversationArea/components/TextMessage/TextMessage.tsx
+++ src/common-components/ConversationArea/components/TextMessage/TextMessage.tsx
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import useAnimatedText from '../../utilities/useAnimatedText';
 
-const TextMessage = ({ text, messageRole, setIsLoading }: TextMessage) => {
+const TextMessage = ({ text, messageRole, setIsLoading }:TextMessage) => {
   const animationSpeed = 50; // Ajustar este valor para controlar la velocidad de la animación
   const displayText = useAnimatedText(text, animationSpeed);
-  const displayTextWithLineBreaks = displayText.split('\n').map((line, index) => (
-    <React.Fragment key={index}>
-      {line}
-      <br />
-    </React.Fragment>
-  ));
+
+  const displayTextWithLineBreaks = useMemo(() => (
+    displayText.split('\n').map((line, index) => (
+      <React.Fragment key={index}>
+        {line}
+        <br />
+      </React.Fragment>
+    ))
+  ), [displayText]);
 
   useEffect(() => {
     if (messageRole === "assistant") {
@@ -17,12 +20,12 @@ const TextMessage = ({ text, messageRole, setIsLoading }: TextMessage) => {
     }
   }, [messageRole, setIsLoading]);
 
+  const messageClassName = `mb-2 rounded px-3 py-2 ${
+    messageRole === "user" ? "bg-blue-500" : "bg-white text-black"
+  }`;
+
   return (
-    <div
-      className={`mb-2 rounded px-3 py-2 ${
-        messageRole === "user" ? "bg-blue-500" : "bg-white text-black"
-      }`}
-    >
+    <div className={messageClassName}>
       {messageRole === "user" ? text : displayTextWithLineBreaks}
     </div>
   );
